1b80d23c6567144a49642fa55ae5e84b61b94313
Eugen <jin.synops@gmail.com>
2021-11-25 13:10:59 +0530

    init]

---
+++ js/main.js
@@ -0,0 +1,24 @@
+$(function() {
+
+    const tpl = article => `
+        <div class="col">
+            <div class="card" style="width: 18rem;">
+                <a href="${article.path}"><img src="${article.img}" class="card-img-top" alt="..."></a>
+                <div class="card-body">
+                    <h4><a href="${article.path}">${article.title}</a></h4>
+                    <p class="card-text"><a href="${article.path}">${article.description}</a></p>
+                </div>
+            </div>
+        </div>
+    `
+
+    $.getJSON('/articles/index.json', function(data, status){
+        const container = $('#article-container');
+        container.html(data.slice(0, 4).map(tpl).join(''))
+    });
+
+    $(document).on('click', '[data-toggle="lightbox"]', function(event) {
+        event.preventDefault();
+        $(this).ekkoLightbox();
+    });
+});
