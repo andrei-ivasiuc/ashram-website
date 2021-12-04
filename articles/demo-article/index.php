ff342c56a887bf97e86385ac55e943f625e8d0e5
Eugen <jin.synops@gmail.com>
2021-12-04 15:19:05 +0530

    demo article

---
+++ articles/demo-article/index.php
@@ -0,0 +1,14 @@
+<?php require_once('../../header.php') ?>
+<?php require_once('../../gallery.php') ?>
+
+<section class="sa-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h2 class="lg mb-4">IE & Mathematics Workshop</h2>
+                <p class="sa-img"><img src="http://sriaurobindoashram.net/social-initiative/7.jpg" alt="" ></p>
+                <p>Sri Aurobindo believed that the focus of Science should be to openly search for and examine non-material, rational system of thoughts rather than insist on forcing the facts into the old material formula. His focus was on looking at Science in a more holistic and integral light. To create and expand this understanding of Science, Sri Aurobindo Ashram Delhi Branch proposes to initiate ‘Integral Education and Science’ Workshop to help teachers from the rural schools develop new skills, inquiring minds, curiosity about science and instill confidence in them about the subject.</p>
+            </div>
+        </div>
+    </div>
+</section>
