4cbbc4256aeba9f12a00efd2935e9d9779650650
ba-sudha <95018790+ba-sudha@users.noreply.github.com>
2021-12-20 14:57:43 +0530

    donations

--- donations/foreign-curr.php
+++ donations/foreign-curr.php
@@ -1,31 +1,93 @@
-BanK aCCoUnTS deTaIlS FoR donaTIonS In FoReIGn
-CURRenCy
-Beneficiary: Sri Aurobindo Ashram – Delhi Branch Trust
-Beneficiary’s Bank Account No. 40034542129 (FCRA Savings A/C)
-Beneficiary’s Address: Sri Aurobindo Marg, New Delhi 110016
-E-mail: contact@aurobindoonline.in
-PAN: AAATS4031K
-FOREIGN INWARD REMITTANCE
-FCRA Cell, 4th Floor, State Bank of India
-New Delhi Main Branch
-11 Sansad Marg (Parliament Street)
-New Delhi 110001
-SWIFT CODE: SBININBB104
-Further credit to Beneficiary Account No. 40034542129 with E/C (whose
-IFSC Code is: SBIN0000691)
-STATE BANK OF INDIA – Branch Address:
-New Delhi Main Branch
-FCRA Division
-11 Sansad Marg (Parliament Street)
-New Delhi 110001
-(Branch Code 00691)
-IMPORTANT NOTE
-1. If you are making direct deposit in our above bank accounts,
-kindly send the copy of the SWIFT MESSAGE which is received at your
-Branch to contact@aurobindoonline.in
-2. Please Provide Transaction, Cheque No., Amount, Bank, Date of
-Deposit.
-3. Please provide your Name, Full Postal Address, Passport No. for
-issue of receipt.
-4. This will help us Reconcile your amount with our bank account
-and issuing official receipt. 
+<?php require_once('../header.php') ?>
+
+<section class="sa-section">
+    <div class="container justify-content-center">
+        <div class="row gx-5 justify-content-center">
+            <div class="col-lg-8 col-sm-12 fs-5 p-5">
+                <h2 style="text-align:center">Donations In Foreign Currency</h2>
+                <h3 class="mb-4">Bank Details of Sri Aurobindo Ashram - Delhi Branch</h3>
+                <table class="table">
+                    <tbody>
+                        <tr>
+                            <th scope="row">email :</th>
+                            <td>contact@aurobindoonline.in</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Name of the Beneficiary :</th>
+                            <td>Sri Aurobindo Ashram - Delhi Branch Trust</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Address of Beneficiary:</th>
+                            <td>
+                                <br>Sri Aurobindo Marg
+                                <br>New Delhi-110016
+                            </td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Beneficiary's Bank Account No. </th>
+                            <td>40034542129 (FCRA Savings A/C)</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">IFSC Code : </th>
+                            <td>SBIN0000691</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">PAN Code :</th>
+                            <td> AAATS4031K</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="sa-section">
+    <div class="container justify-content-center">
+        <div class="row gx-5 justify-content-center">
+            <div class="col-12 col-lg-8 col-md-6">
+                <!-- <div class="col-lg-10 col-sm-12 fs-5 p-5"> -->
+                <h2 style="text-align:center">FOREIGN INWARD REMITTANCE</h2>
+                <div class="card">
+                    <p> State Bank of India - New Delhi Main Branch </p>
+                    <br>FCRA Cell, 4th Floor,
+                    <br>11 Sansad Marg (Parliament Street)
+                    <br>New Delhi - 110 001
+                    <br>SWIFT Code : SBININBB104
+                </div>
+            </div>
+            <div class="col-12 col-lg-8 col-md-6">
+                <div class="card">
+                    <p>
+                        Further credit to Beneficiary Account No. 40034542129 with E/C (whose
+                        IFSC Code is: SBIN0000691)</p>
+                    <br>State Bank of India
+                    <br>New Delhi Main Branch
+                    <br>FCRA Cell, 4th Floor,
+                    <br>11 Sansad Marg (Parliament Street)
+                    <br>New Delhi - 110 001
+                    <br>(Branch Code : 00691)
+                    <br>SWIFT Code : SBININBB104
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="sa-section">
+    <div class="container justify-content-center">
+        <div class="row gx-5 justify-content-center" ;>
+            <div class="col-lg-8 col-sm-12 fs-5 p-5">
+                <h4 class="mb-4" style="text-align:center"> <b>IMPORTANT NOTE </b> </h4>
+                <p>1. If you are making direct deposit in our above bank accounts,
+                    kindly send the copy of the SWIFT MESSAGE which is received at your
+                    Branch to contact@aurobindoonline.in</p>
+                <p>2. Please Provide Transaction, Cheque No., Amount, Bank, Date of
+                    Deposit.</p>
+                <p>3. Please provide your Name, Full Postal Address, Passport No. for
+                    issue of receipt.</p>
+                <p>4. This will help us Reconcile your amount with our bank account
+                    and issuing official receipt..</p>
+                <p>
+            </div>
+        </div>
+    </div>
+</section>
---
+++ sponsorship-schemes/index.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php require_once('../header.php') ?>
+<body>
+    <section class="header-horizontal dark" style="background-color: var(--bg-darker);">
+        <div class="text-center mb-4">
+            <h2>SUPPORTING AND PROMOTING EDUCATION</h2>
+            <img src="http://www.sriaurobindoashram.net/VT/Untitled-10.PNG" class="mb-4">
+        </div>
+    </section>
+    <section class="event-text">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-8 col-sm-12 fs-5 p-5">
+                    <h4>
+                        What we are attempting here is to prove to the world, through a concrete example,
+                        that by some inner psychological realisation and some outer organisation a world
+                        can be created where most of the causes of human misery will cease to exist.
+                    </h4>
+                    <br>
+                    <h5>The Mother</h5>
+                    <br>
+                    <p>
+                        We, at Sri Aurobindo Ashram - Delhi branch, are essentially focused on supporting
+                        education as we strongly believe that giving integral education to a child is our greatest
+                        achievement. It is our mission to touch as many lives as possible. To garner support for this
+                        cause we have initiated various sponsorship schemes from individuals and the corporate
+                        sector, which have gained momentum in the past years.
+                        This year the pandemic has created unprecedented situation for the students as well
+                        as the teachers. The past few months have been difficult for everyone and specially for
+                        students. We have witnessed that students across all schools are demonstrating incredible
+                        energy, commitment and flexibility as they continue to respond to the mass disruption
+                        caused by the pandemic.
+                    </p>
+                    <p>
+                        Our schools have adapted to new teaching techniques with virtual lessons. The disruption
+                        caused by the pandemic has created a need for teachers to be more creative than usual,
+                        employing new measures and in some cases developing completely new skills sets to
+                        ensure the successful continuation of the teaching-learning process.
+                        Many corporate and individual sponsors have appreciated our work and extended
+                        financial support to us to enable us to overcome this crisis. The children of our schools
+                        in rural India benefited and their numbers continue to grow. The children in remote areas,
+                        tribal population and villages in the hilly areas are being educated under these schemes.
+                        The sponsored children are selected from low income groups and educated in good village
+                        boarding schools in their own state to avoid language problems. The entire expense of their
+                        tuition, boarding, lodging, training, books, clothes, travel and medical expenses is borne
+                        by the Sri Aurobindo Ashram - Delhi Branch. Additionally, a few students in The Mother’s
+                        International School, from the economically weaker section are also provided scholarships
+                        under the sponsorship programme.
+                    </p>
+                    <p>
+                        The Auro Mira Vidya Mandir, a free primary school for over 165 tribal children in
+                        Kechla, Odisha is also mainly supported by Sri Aurobindo Ashram-Delhi branch as part of
+                        sponsorship programmes. The children preferred to stay in the school hostel during these
+                        tough times and were kept in very safe and healthy surroundings. All safety precautions to
+                        protect against Covid 19 were followed.
+                    </p>
+                    <p>
+                        The sponsorship schemes have been largely used to support children and youth mainly
+                        under the following four main categories:
+                    </p>
+                    <h4>
+                        School Education
+                    </h4>
+                    <p>
+                        There are more than 500 Sri Aurobindo Schools all over the country which are run by
+                        devotees independently. A number of children in these schools are sponsored by the Sri
+                        Aurobindo Ashram - Delhi branch. These children are provided with books, clothing, games
+                        and sports equipment among other essential items from time to time.
+                        At the end of the academic year, the sponsors are provided with the progress report of
+                        each child supported by them.
+                    </p>
+                    <h4>
+                        Higher Education
+                    </h4>
+                    <p>
+                        Many of our Ashram Youth after finishing class XII are pursuing their online graduation in
+                        different streams. We sponsor their undergraduate studies and thereafter career-oriented
+                        courses.
+                    </p>
+                    <h4>
+                        Vocational Training Program
+                    </h4>
+                    <p>
+                        Sri Aurobindo Ashram-Delhi branch also has a vocational training programme for the
+                        youth of weaker strata of the society to upgrade their skills and knowledge. As part of this
+                        programme, the youth undergo structured training of six months in the areas of cooking,
+                        carpentry, photo-framing and lamination, hand-made paper making, tailoring and weaving
+                        (for girls), civil and electrical maintenance, library science, computer and typing, and paramedical training.
+                        After the successful completion of the course, most trainees could be successfully placed
+                        in jobs. A few trainees, however, continue to work in the Ashram in various departments.
+                        The Ashram extends financial support to them and they are encouraged to take up courses
+                        of their choice. During 2020-21 there were on an average 28 vocational trainees in the
+                        Ashram.
+                    </p>
+                    <p>
+                        Scholarships for School Education, Higher Education,
+                        Vocational Training, Art, Music & Yoga
+                    </p>
+                    <p>
+                        The details of amount spent under various categories in the year 2020-2021 are as under:
+                    </p>
+                    <p>
+                        Details of Program Number of students Amount (INR)
+                        School Education 161 56,98,000/-
+                        Higher Education & other
+                        courses of Music, Art & Driving
+                        17 1,15,805/-
+                        Vocational Training Program 28 2,72,100/-
+                        TOTAL 206 60,85,905/
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
