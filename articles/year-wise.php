ce67628badd080801056ac535827422cb0ee12ea
sudha-laptop <basudha212@gmail.com>
2021-12-30 16:23:46 +0530

    year-wise

---
+++ articles/year-wise.php
@@ -0,0 +1,36 @@
+<?php require_once('../header.php') ?>
+
+<section class="sa-section">
+    <div class="container" >
+        <div class="row">
+            <nav class="navbar navbar-expand-lg navbar-dark"  style="background-color:var(--gold)">
+                <div class="container">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                 
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle active" href="#" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                           Yoga
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarScrollingDropdown">
+                      
+                            <li><a class="dropdown-item" href="/ashram/history">2021</a></li>
+                            <li><a class="dropdown-item" href="/ashram/facilities">2020</a></li>
+                            <li><a class="dropdown-item" href="/ashram/vocational-training">Vocational Training</a></li>
+                        </ul>
+                    </li>
+
+                                        
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    </div>
+</section>
+
+<?php require_once('../footer.php') ?>
